e6d9745d81d27bf973a1decda054aa18eb626496
joaorema <joaopcrema@gmail.com>
2025-03-12 11:49:26 +0000

    testeteste

--- minishell.h
+++ minishell.h
@@ -1,4 +1,21 @@
-teste 1 
-teste 2
-teste 3 
-teste 4
+
+#define EXEC 1
+#define REDIRECT 2
+#define PIPE 3
+#define LIST 4
+#define BACK 5
+
+#define MAXARGS 10   //MAX nbr of arguments we can handle
+
+struct cmd
+{
+    int type;
+};
+
+struct execcmd
+{
+    int type;
+    char *argv[MAXARGS];    //Ptr to start of string
+    char *earg[MAXARGS];    //Ptr to end of string to put '\0'
+     
+};
