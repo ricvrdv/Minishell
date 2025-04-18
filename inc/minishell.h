46104b93043a816556da8d638f739aad2b07b52e
joaorema <joaopcrema@gmail.com>
2025-04-18 11:02:01 +0100

    refactor exec pipe to have 25lines

--- inc/minishell.h
+++ inc/minishell.h
@@ -214,5 +214,7 @@ void    remove_quotes(char *arg, s_tree *tree);
 
 bool	should_expand(const char *str);
 bool	enclosed_single_quotes(const char *str);
+int     pipe_and_fork(int *pipefd);
+void    child_process(s_tree *node, s_minishell *mini, int *pipefd, int dir);
 
 #endif
